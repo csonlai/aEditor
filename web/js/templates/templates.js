f7e1ccbbdc3bf2827d7e0d24399058af722b6bae
csonlai <1024062499@qq.com>
2015-06-26 15:19:47 +0800

    更新

--- web/js/templates/templates.js
+++ web/js/templates/templates.js
@@ -323,9 +323,9 @@ this["JST"]["work_list_item"] = Handlebars.template({"1":function(depth0,helpers
     + escapeExpression(((helper = (helper = helpers.id || (depth0 != null ? depth0.id : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"id","hash":{},"data":data}) : helper)))
     + "\" data-name=\""
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "\">\n		<button class=\"work-delete-btn glyphicon glyphicon-remove\"></button>\n		"
+    + "\">\n		<button class=\"work-delete-btn glyphicon glyphicon-remove\"></button>\n		<div class=\"list-work-name\">"
     + escapeExpression(((helper = (helper = helpers.name || (depth0 != null ? depth0.name : depth0)) != null ? helper : helperMissing),(typeof helper === functionType ? helper.call(depth0, {"name":"name","hash":{},"data":data}) : helper)))
-    + "\n		<div class=\"glyphicon glyphicon-link\"></div>\n	</a>\n";
+    + "</div>\n		<div class=\"glyphicon glyphicon-link\"></div>\n	</a>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
   var stack1;
   stack1 = helpers.each.call(depth0, (depth0 != null ? depth0.workList : depth0), {"name":"each","hash":{},"fn":this.program(1, data),"inverse":this.noop,"data":data});
